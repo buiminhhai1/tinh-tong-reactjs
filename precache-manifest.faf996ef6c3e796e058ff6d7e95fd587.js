8cd3280cff0d64dc1ad4028ab3cb2c9479d485cc
buiminhhai1 <ngovietduc20088@gmail.com>
2019-09-06 18:09:38 +0700

    Updates

---
+++ precache-manifest.faf996ef6c3e796e058ff6d7e95fd587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aec0b667dea78640be9ba2375683138",
+    "url": "/buiminhhai1/tinh-tong-reactjs.git/index.html"
+  },
+  {
+    "revision": "c04e7c0215315603cbf1",
+    "url": "/buiminhhai1/tinh-tong-reactjs.git/static/css/main.1818040d.chunk.css"
+  },
+  {
+    "revision": "c5cf771c19208756910c",
+    "url": "/buiminhhai1/tinh-tong-reactjs.git/static/js/2.80825205.chunk.js"
+  },
+  {
+    "revision": "c04e7c0215315603cbf1",
+    "url": "/buiminhhai1/tinh-tong-reactjs.git/static/js/main.3093301d.chunk.js"
+  },
+  {
+    "revision": "4e297d04c4fddf1995b9",
+    "url": "/buiminhhai1/tinh-tong-reactjs.git/static/js/runtime~main.69672e2c.js"
+  }
+]);
